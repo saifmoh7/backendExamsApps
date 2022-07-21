d2a9904cca33507d4bd607b87fa3df5480208f5a
saifmoh7 <saif.2017.moh@gmail.com>
2022-07-21 21:35:09 +0300

    modfi router

--- routers/questionRouter.js
+++ routers/questionRouter.js
@@ -7,32 +7,71 @@ const questionRouter = express.Router();
 
 questionRouter.post('/addquestion', expressAsyncHandler(async (req, res) => {
     const exam = await Exam.findOne({examId: req.body.examId})
+    
     const question = {
-        question : req.body.question
+        _id: stringGenerator(16),
+        question : req.body.question,
+        questionImageUrl: req.body.questionImageUrl,
+        trueOrFalse: req.body.trueOrFalse,
+        correctAnswer: req.body.option_1,
+        option_1: req.body.option_1,
+        option_2: req.body.option_2,
+        option_3: req.body.option_3,
+        option_4: req.body.option_4
     }
     exam.questions.push(question)
-    const createdExam = await exam.save();
-    res.json({createdExam})
-    console.log(createdExam)
-    
-    // const question = Question({
-    //     examId: req.body.examId,
-    //     question: req.body.question,
-    //     questionImage: req.body.questionImage,
-    //     fourChoice: req.body.fourChoice,
-    //     correctAnswer: req.body.correctAnswer,
-    //     option_1: req.body.option_1,
-    //     option_2: req.body.option_2,
-    //     option_3: req.body.option_3,
-    //     option_4: req.body.option_4
-    // })
-    // const createdQuestion = await question.save();
-    // if (createdQuestion) {
-    //     res.json({status:200,
-    //         question: createdQuestion.question
-    //     })
-    // }else {res.json({status: 403})}
+    const addQustion = await exam.save();
+    res.json({status:200, addQustion})
 }))
+
+questionRouter.post('/showquestions', expressAsyncHandler(async (req, res) => {
+    try {
+        const exam = await Exam.findOne({_id: req.body.examId})
+        const questions = exam.questions
+
+        if (questions) {
+            res.json({status:200, questions})
+        } else {res.json({status:403, message: "Exam Not Found"})}
+    } catch (error) {
+        res.json({status:403, error})
+    }
+}))
+
+questionRouter.post('/deleteques', expressAsyncHandler(async (req, res) => {
+    try {
+        const deletQues = await Exam.updateOne({_id: req.body.examId}, {$pull: {"questions": {_id: req.body.quesId}}})
+        // const questions = exam.questions
+        // const deletQues = questions.filter(Object => Object._id===req.body.quesId)
+
+
+        if (deletQues) {
+            res.json({status:200, message: "Question is Deleted"})
+        } else {res.json({status:403, message: "Exam Not Found"})}
+    } catch (error) {
+        res.json({status:403, error})
+    }
+}))
+
+
+
+function stringGenerator(length=10,options={}){
+    let { onlyNumbers } = options;
+    let symboles = ['A','a','B','b','C','c','D','d','E','e','F','f','G','g','H','h','I','i','J','j','K','k','L','l','M','m','N','n','O',
+                    'o','P','p','Q','q','R','r','S','s','T','t','U','u','V','v','W','w','X','x','Y','y','Z','z','0','1','2','3','4','5','6','7','8','9'];
+    if( onlyNumbers === true ){
+      symboles = ['0','1','2','3','4','5','6','7','8','9'];
+    }
+    let string = '';
+    for (let i = 0; i < parseInt(length); i++) {
+      let index = Math.floor(Math.random()*symboles.length);
+      string+=symboles[index]
+    }
+    return string;
+  }
+
+
+module.exports = questionRouter;
+
 
 
 // examRouter.post('/addexam', expressAsyncHandler(async (req, res) => {
@@ -55,4 +94,21 @@ questionRouter.post('/addquestion', expressAsyncHandler(async (req, res) => {
 //     } else {res.json({status: 403})}
 // }))
 
-module.exports = questionRouter;
+
+    // const question = Question({
+    //     examId: req.body.examId,
+    //     question: req.body.question,
+    //     questionImage: req.body.questionImage,
+    //     fourChoice: req.body.fourChoice,
+    //     correctAnswer: req.body.correctAnswer,
+    //     option_1: req.body.option_1,
+    //     option_2: req.body.option_2,
+    //     option_3: req.body.option_3,
+    //     option_4: req.body.option_4
+    // })
+    // const createdQuestion = await question.save();
+    // if (createdQuestion) {
+    //     res.json({status:200,
+    //         question: createdQuestion.question
+    //     })
+    // }else {res.json({status: 403})}
